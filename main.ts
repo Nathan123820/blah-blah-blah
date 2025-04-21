88d8945e624f6cb53a96475a8db754cef66a690a
Nathan123820 <cmckinnie@hillsidesdaschool.org>
2025-04-21 08:25:41 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,103 @@
+let index = 0
+let list: number[] = []
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+    led.plotBarGraph(
+    0,
+    0
+    )
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    radio.sendNumber(0)
+    radio.sendValue("The NeoSmart Files https://neosmart.net/blog Recovery software and more Wed, 10 Jul 2024 20:13:45 +0000 en-US hourly 1 https://wordpress.org/?v=6.5.5 https://neosmart.net/blog/wp-content/uploads/cropped-NeoSmart-Padded-32x32.png The NeoSmart Files https://neosmart.net/blog 32 32 1082592 Benchmarking rust compilation speedups and slowdowns from sccache and -Zthreads https://neosmart.net/blog/benchmarking-rust-compilation-speedups-and-slowdowns-from-sccache-and-zthreads/ https://neosmart.net/blog/benchmarking-rust-compilation-speedups-and-slowdowns-from-sccache-and-zthreads/#comments  Mon, 01 Jul 2024 18:50:08 +0000       https://neosmart.net/blog/?p=5166   https://neosmart.net/blog/benchmarking-rust-compilation-speedups-and-slowdowns-from-sccache-and-zthreads/feed/ 2 5166 Using build.rs to integrate rust applications with system libraries like a pro https://neosmart.net/blog/using-build-rs-to-integrate-rust-applications-with-system-libraries-like-a-pro/ https://neosmart.net/blog/using-build-rs-to-integrate-rust-applications-with-system-libraries-like-a-pro/#respond  Mon, 13 May 2024 16:24:30 +0000       https://neosmart.net/blog/?p=5136   https://neosmart.net/blog/using-build-rs-to-integrate-rust-applications-with-system-libraries-like-a-pro/feed/ 0 5136 Embed only the video from another post on X or Twitter https://neosmart.net/blog/embed-only-the-video-from-another-post-on-x-or-twitter/ https://neosmart.net/blog/embed-only-the-video-from-another-post-on-x-or-twitter/#comments  Sun, 22 Oct 2023 17:33:56 +0000    https://neosmart.net/blog/?p=5104   https://neosmart.net/blog/embed-only-the-video-from-another-post-on-x-or-twitter/feed/ 2 5104 Increment only numbers matching regex in Vim https://neosmart.net/blog/increment-only-numbers-matching-regex-in-vim/ https://neosmart.net/blog/increment-only-numbers-matching-regex-in-vim/#comments  Fri, 13 Oct 2023 17:47:14 +0000     https://neosmart.net/blog/?p=5071   https://neosmart.net/blog/increment-only-numbers-matching-regex-in-vim/feed/ 4 5071 tcpproxy 0.4 released https://neosmart.net/blog/tcpproxy-0-4-released/ https://neosmart.net/blog/tcpproxy-0-4-released/#respond  Sun, 08 Oct 2023 18:53:36 +0000       https://neosmart.net/blog/?p=5060   https://neosmart.net/blog/tcpproxy-0-4-released/feed/ 0 5060 CallerArgumentExpression and extension methods don’t mix https://neosmart.net/blog/callerargumentexpression-and-extension-methods-dont-mix/ https://neosmart.net/blog/callerargumentexpression-and-extension-methods-dont-mix/#respond  Mon, 11 Sep 2023 17:17:55 +0000         https://neosmart.net/blog/?p=5037   https://neosmart.net/blog/callerargumentexpression-and-extension-methods-dont-mix/feed/ 0 5037 Implementing truly safe semaphores in rust https://neosmart.net/blog/implementing-truly-safe-semaphores-in-rust/ https://neosmart.net/blog/implementing-truly-safe-semaphores-in-rust/#comments  Mon, 03 Oct 2022 20:11:34 +0000        https://neosmart.net/blog/?p=4958   https://neosmart.net/blog/implementing-truly-safe-semaphores-in-rust/feed/ 4 4958 SecureStore 0.100: KISS, git-versioned secrets management for rust https://neosmart.net/blog/securestore-0-100-kiss-git-versioned-secrets-management-for-rust/ https://neosmart.net/blog/securestore-0-100-kiss-git-versioned-secrets-management-for-rust/#respond  Mon, 08 Aug 2022 19:00:15 +0000        https://neosmart.net/blog/?p=4940   https://neosmart.net/blog/securestore-0-100-kiss-git-versioned-secrets-management-for-rust/feed/ 0 4940 C# file size formatting library PrettySize 3.1 released https://neosmart.net/blog/c-file-size-formatting-library-prettysize-3-1-0-released/ https://neosmart.net/blog/c-file-size-formatting-library-prettysize-3-1-0-released/#respond  Mon, 27 Jun 2022 16:59:00 +0000        https://neosmart.net/blog/?p=4925   https://neosmart.net/blog/c-file-size-formatting-library-prettysize-3-1-0-released/feed/ 0 4925 PrettySize 0.3 release and a weakness in rust’s type system https://neosmart.net/blog/prettysize-0-3-release-and-a-weakness-in-rusts-type-system/ https://neosmart.net/blog/prettysize-0-3-release-and-a-weakness-in-rusts-type-system/#comments  Wed, 22 Jun 2022 23:07:06 +0000        https://neosmart.net/blog/?p=4901   https://neosmart.net/blog/prettysize-0-3-release-and-a-weakness-in-rusts-type-system/feed/ 2 4901 Easy Window Switcher 1.3.0 released https://neosmart.net/blog/easy-window-switcher-1-3-0-released/ https://neosmart.net/blog/easy-window-switcher-1-3-0-released/#comments  Sat, 05 Feb 2022 20:50:12 +0000     https://neosmart.net/blog/?p=4896   https://neosmart.net/blog/easy-window-switcher-1-3-0-released/feed/ 10 4896 CVE-2022-23968: Xerox vulnerability allows unauthenticated users to remotely brick network printers (UPDATED) https://neosmart.net/blog/xerox-vulnerability-allows-unauthenticated-network-users-to-remotely-brick-printers/ https://neosmart.net/blog/xerox-vulnerability-allows-unauthenticated-network-users-to-remotely-brick-printers/#respond  Mon, 24 Jan 2022 22:31:07 +0000       https://neosmart.net/blog/?p=4865   https://neosmart.net/blog/xerox-vulnerability-allows-unauthenticated-network-users-to-remotely-brick-printers/feed/ 0 4865 Free Windows 11 Repair and Recovery Tool Download https://neosmart.net/blog/free-windows-11-repair-and-recovery-tool-download/ https://neosmart.net/blog/free-windows-11-repair-and-recovery-tool-download/#comments  Tue, 04 Jan 2022 22:49:54 +0000       https://neosmart.net/blog/?p=4833   https://neosmart.net/blog/free-windows-11-repair-and-recovery-tool-download/feed/ 10 4833 Microsoft bids adieu to Windows Phone in new emoji https://neosmart.net/blog/microsoft-bids-adieu-to-windows-phone-in-new-emoji/ https://neosmart.net/blog/microsoft-bids-adieu-to-windows-phone-in-new-emoji/#comments  Tue, 02 Nov 2021 18:17:48 +0000       https://neosmart.net/blog/?p=4813   https://neosmart.net/blog/microsoft-bids-adieu-to-windows-phone-in-new-emoji/feed/ 2 4813 Using SIMD acceleration in rust to create the world’s fastest tac https://neosmart.net/blog/using-simd-acceleration-in-rust-to-create-the-worlds-fastest-tac/ https://neosmart.net/blog/using-simd-acceleration-in-rust-to-create-the-worlds-fastest-tac/#respond  Mon, 30 Aug 2021 18:57:07 +0000        https://neosmart.net/blog/?p=4801   https://neosmart.net/blog/using-simd-acceleration-in-rust-to-create-the-worlds-fastest-tac/feed/ 0 4801", 25)
+})
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    index += 1
+})
+radio.onReceivedValue(function (name, value) {
+	
+})
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+loops.everyInterval(500, function () {
+    index += 1
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+})
+loops.everyInterval(500, function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+})
+loops.everyInterval(500, function () {
+	
+})
+loops.everyInterval(500, function () {
+	
+})
+loops.everyInterval(500, function () {
+    led.plotBarGraph(
+    0,
+    0
+    )
+    continue;
+})
+loops.everyInterval(500, function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    music.ringTone(262)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.rest(music.beat(BeatFraction.Whole))
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    if (true) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+        for (let value of list) {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+        }
+    } else {
+        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+loops.everyInterval(500, function () {
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+})
